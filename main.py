304c7b025c256d4471d842073afecb034bccb1e3
CCat <61354181+CCathlete@users.noreply.github.com>
2024-04-07 13:47:17 +0000

    Removed the use of beautiful soup.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from seleniumhandler import SeleniumHandler
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+# From selenium.webdriver.common.keys import Keys #Only needed if I want
+# to press enter.
 import time
-from bs4 import BeautifulSoup
 
 my_selenium = SeleniumHandler()
-# create a driver to get URLs and perform other actions
+# Create a driver to get URLs and perform other actions
 my_selenium.new_driver()
 driver = my_selenium.driver
-# driver.maximize_window() Check if needed.
+# Driver.maximize_window() Check if needed.
 
-# getting the url of the mobile search page in twitch.
+# Getting the url of the mobile search page in twitch.
 my_selenium.get("https://m.twitch.tv")
 
 search_button = driver.find_element(
@@ -21,33 +21,17 @@ search_button.click()
 search_bar = driver.find_element(
     By.XPATH, '/html/body/div/div/nav/div/div/div[2]/div/div/input'
 )
-# typing our search word.
+# Typing our search word.
 search_bar.send_keys("starcraft II")
 time.sleep(5)
 
-# finding the element of the first search result.
+# Finding the element of the first search result.
 search_result = driver.find_element(
     By.XPATH, '//*[@id="__next"]/div/main/div/div/ul/li[1]/a/div/p'
 )
 search_result.click()
 # search_bar.send_keys(Keys.RETURN)
 
-# allow time for dynamic elements to load
-# this just waits a max of 10 seconds or until all scripts are done running
-driver.implicitly_wait(10)
-# I've also found that a time.sleep() works well here
-# uncomment if results are not what is expected
-time.sleep(2)
+# Waits 10 seconds or until all scripts are done running
+time.sleep(10)
 
-# function packaged into get_html() in seleniumhandler
-# takes in a URL and returns the page's source (HTML)
-# usage: html = get_html("https://google.com", driver)
-html = driver.page_source
-soup = BeautifulSoup(html, "html.parser")
-
-first_result = soup.find("h3", {"class": "LC20lb MBeuO DKV0Md"})
-print("First result: '" + first_result.get_text() + "'")
-
-# text = soup.get_text()
-# print("All text:")
-# print(text)
